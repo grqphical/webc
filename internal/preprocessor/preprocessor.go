0b51c1650b7466c2a97c34548028a956dfa9f3bc
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2026-02-06 21:17:42 -0800

    got if-else statements working with preprocessor

--- internal/preprocessor/preprocessor.go
+++ internal/preprocessor/preprocessor.go
@@ -6,7 +6,9 @@ import (
 )
 
 type Preprocessor struct {
-	Definitions map[string]string
+	Definitions       map[string]string
+	includeStatements bool
+	inIfStatement     bool
 }
 
 func New() *Preprocessor {
@@ -16,31 +18,69 @@ func New() *Preprocessor {
 	defs["__webc__"] = "1"
 
 	return &Preprocessor{
-		Definitions: defs,
+		Definitions:       defs,
+		includeStatements: true,
+		inIfStatement:     false,
 	}
 }
 
-func (p *Preprocessor) Parse(sourceCode string) error {
+func (p *Preprocessor) Parse(sourceCode string) (string, error) {
+	finalSource := ""
 	for _, line := range strings.Split(sourceCode, "\n") {
 		// remove trailing whitespace
 		line := strings.TrimLeft(line, " \t")
 
-		if !(line[0] == '#') {
+		if len(line) < 1 {
 			continue
+		}
+
+		if !(line[0] == '#') {
+			if p.includeStatements {
+				finalSource += line + "\n"
+			}
 		}
 
 		if strings.HasPrefix(line, "#define") {
 			split := strings.Split(line, " ")
 			if len(split) < 3 {
-				return errors.New("invalid preprocessor statement")
+				return "", errors.New("invalid preprocessor statement")
 			}
 
 			name := split[1]
 			value := strings.Join(split[2:], " ")
 
 			p.Definitions[name] = value
+		} else if strings.HasPrefix(line, "#ifdef") {
+			p.inIfStatement = true
+			split := strings.Split(line, " ")
+			if len(split) < 2 {
+				return "", errors.New("invalid preprocessor statement")
+			}
+
+			definition := split[1]
+			if _, ok := p.Definitions[definition]; !ok {
+				p.includeStatements = false
+			}
+		} else if strings.HasPrefix(line, "#ifndef") {
+			p.inIfStatement = true
+			split := strings.Split(line, " ")
+			if len(split) < 2 {
+				return "", errors.New("invalid preprocessor statement")
+			}
+
+			definition := split[1]
+			if _, ok := p.Definitions[definition]; ok {
+				p.includeStatements = false
+			}
+		} else if strings.HasPrefix(line, "#else") {
+			p.includeStatements = !p.includeStatements
+		} else if strings.HasPrefix(line, "#endif") {
+			if !p.inIfStatement {
+				return "", errors.New("invalid preprocessor if statement")
+			}
+			p.includeStatements = true
 		}
 	}
 
-	return nil
+	return finalSource, nil
 }
--- internal/preprocessor/preprocessor_test.go
+++ internal/preprocessor/preprocessor_test.go
@@ -12,11 +12,34 @@ func TestDefinitions(t *testing.T) {
 	#define BARFOO Hello World`
 
 	pp := preprocessor.New()
-	err := pp.Parse(input)
+	_, err := pp.Parse(input)
 	assert.NoError(t, err)
 
 	assert.Equal(t, 4, len(pp.Definitions)) // 2 new ones plus 2 default ones
 
 	assert.Equal(t, "1", pp.Definitions["FOOBAR"])
 	assert.Equal(t, "Hello World", pp.Definitions["BARFOO"])
+}
+
+func TestIfStatements(t *testing.T) {
+	input := `#define FOOBAR 1
+	#ifdef FOOBAR
+	blah blah blah
+	#endif
+	#ifndef FOOBAR
+	boo boo boo
+	#else
+	blah blah blah
+	#endif
+	boo
+`
+
+	pp := preprocessor.New()
+	source, err := pp.Parse(input)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 3, len(pp.Definitions)) // 1 new one plus 2 default ones
+
+	assert.Equal(t, "1", pp.Definitions["FOOBAR"])
+	assert.Equal(t, "blah blah blah\nblah blah blah\nboo\n", source)
 }
