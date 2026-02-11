4c2499f82ed2e71ae6c649ac904ee25ec4f0b1db
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2026-02-10 18:56:10 -0800

    fix: updated tests so that pre processor tests will pass

--- internal/preprocessor/preprocessor_test.go
+++ internal/preprocessor/preprocessor_test.go
@@ -16,7 +16,7 @@ func TestDefinitions(t *testing.T) {
 	_, err := pp.Parse(input)
 	assert.NoError(t, err)
 
-	assert.Equal(t, 4, len(pp.Definitions)) // 2 new ones plus 2 default ones
+	assert.Equal(t, 8, len(pp.Definitions)) // 2 new ones plus 6 default ones
 
 	assert.Equal(t, "1", pp.Definitions["FOOBAR"])
 	assert.Equal(t, "Hello World", pp.Definitions["BARFOO"])
@@ -39,7 +39,7 @@ func TestIfStatements(t *testing.T) {
 	source, err := pp.Parse(input)
 	assert.NoError(t, err)
 
-	assert.Equal(t, 3, len(pp.Definitions)) // 1 new one plus 2 default ones
+	assert.Equal(t, 7, len(pp.Definitions)) // 1 new one plus 6 default ones
 
 	assert.Equal(t, "1", pp.Definitions["FOOBAR"])
 	assert.Equal(t, "blah blah blah\nblah blah blah\nboo\n", source)
